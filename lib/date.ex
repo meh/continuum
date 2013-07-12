60110c46279612b400c44af3191bef7cfd841297
meh <meh@schizofreni.co>
2013-07-12 16:15:20 +0200

    date: add timezone awareness

--- lib/date.ex
+++ lib/date.ex
@@ -4,14 +4,26 @@ defmodule Date do
   @type day   :: 1 .. 31
   @type week  :: 1 .. 53
 
-  @type t :: { year, month, day }
+  @type t :: { year, month, day } | { Timezone.t, { year, month, day } }
+
+  def valid?({ year, month, day }) do
+    :calendar.valid_date(year, month, day)
+  end
+
+  def valid?({ zone, { year, month, day } }) do
+    Timezone.exists?(zone) and :calendar.valid_date(year, month, day)
+  end
 
   def now do
     :calendar.now_to_datetime(:erlang.now) |> elem(0)
   end
 
-  def valid?({ year, month, day }) do
-    :calendar.valid_date(year, month, day)
+  def timezone({ _, _, _ }) do
+    "UTC"
+  end
+
+  def timezone({ zone, _ }) do
+    zone
   end
 
   def from_epoch(seconds) do
@@ -21,6 +33,13 @@ defmodule Date do
   end
 
   def to_epoch(date) do
+    zone = timezone(date)
+    date = timezone(date, "UTC")
+
+    if date < (DateTime.epoch |> DateTime.date) do
+      raise ArgumentError, message: "cannot convert a date less than 1970-1-1"
+    end
+
     :calendar.datetime_to_gregorian_seconds({ date, { 0, 0, 0 } }) -
       :calendar.datetime_to_gregorian_seconds(DataTime.epoch)
   end
