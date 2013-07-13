a690beec15cddca4793e40adc20be5b66f393ea3
meh <meh@schizofreni.co>
2013-07-14 00:31:47 +0200

    date: add some helpers and specs

--- lib/date.ex
+++ lib/date.ex
@@ -6,6 +6,7 @@ defmodule Date do
 
   @type t :: { year, month, day } | { Timezone.t, { year, month, day } }
 
+  @spec valid?(t) :: boolean
   def valid?({ year, month, day }) do
     :calendar.valid_date(year, month, day)
   end
@@ -14,10 +15,25 @@ defmodule Date do
     Timezone.exists?(zone) and :calendar.valid_date(year, month, day)
   end
 
-  def now do
-    :calendar.now_to_datetime(:erlang.now) |> elem(0)
+  @spec now             :: t
+  @spec now(Timezone.t) :: t
+  def now(zone // "UTC") do
+    :calendar.now_to_datetime(:erlang.now) |> elem(0) |> timezone(zone)
   end
 
+  @spec year(t) :: year
+  def year({ _, time }),    do: year(time)
+  def year({ year, _, _ }), do: year
+
+  @spec month(t) :: month
+  def month({ _, time }),     do: month(time)
+  def month({ _, month, _ }), do: month
+
+  @spec day(t) :: day
+  def day({ _, time }),   do: day(time)
+  def day({ _, _, day }), do: day
+
+  @spec timezone(t) :: Timezone.t
   def timezone({ _, _, _ }) do
     "UTC"
   end
@@ -26,12 +42,32 @@ defmodule Date do
     zone
   end
 
+  # TODO: actually do the date change
+  @spec timezone(t, Timezone.t) :: t
+  def timezone({ _old, date }, new) do
+    if Timezone.equal? new, "UTC" do
+      date
+    else
+      { new, date }
+    end
+  end
+
+  def timezone(date, new) do
+    if Timezone.equal? new, "UTC" do
+      date
+    else
+      { new, date }
+    end
+  end
+
+  @spec from_epoch(non_neg_integer) :: t
   def from_epoch(seconds) do
     (:calendar.datetime_to_gregorian_seconds(DateTime.epoch) + seconds)
       |> :calendar.gregorian_seconds_to_datetime
       |> elem(0)
   end
 
+  @spec to_epoch(t) :: non_neg_integer
   def to_epoch(date) do
     zone = timezone(date)
     date = timezone(date, "UTC")
@@ -44,12 +80,16 @@ defmodule Date do
       :calendar.datetime_to_gregorian_seconds(DataTime.epoch)
   end
 
+  @doc """
+  Using Date will import the `is_date` guards.
+  """
   defmacro __using__(_opts) do
     quote do
       import Date, only: [is_date: 1, is_date: 2]
     end
   end
 
+  @spec is_date(term) :: boolean
   defmacro is_date(var) do
     quote do
       ((tuple_size(unquote(var)) == 3 and elem(unquote(var), 0) > 0 and
@@ -62,6 +102,7 @@ defmodule Date do
     end
   end
 
+  @spec is_date(term, Timezone.t) :: boolean
   defmacro is_date(var, zone) when is_binary(zone) do
     if Timezone.equal? zone, "UTC" do
       quote do
