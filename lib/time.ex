8c24b41655a2bddc62d8b914dac5482db1a9db01
meh <meh@schizofreni.co>
2013-07-14 00:14:09 +0200

    time: use is_timezone in is_time

--- lib/time.ex
+++ lib/time.ex
@@ -55,10 +55,8 @@ defmodule Time do
                                            elem(unquote(var), 2) in 0 .. 59)
       end
     else
-      zones = Timezone.synonyms_for zone
-
       quote do
-        (tuple_size(unquote(var)) == 2 and elem(unquote(var), 0) in unquote(zones) and
+        (tuple_size(unquote(var)) == 2 and is_timezone(elem(unquote(var), 0), unquote(zone)) and
           tuple_size(elem(unquote(var), 0)) == 3 and elem(elem(unquote(var), 1), 0) in 0 .. 23 and
                                                      elem(elem(unquote(var), 1), 1) in 0 .. 59 and
                                                      elem(elem(unquote(var), 1), 2) in 0 .. 59)
