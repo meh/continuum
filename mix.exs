138be7fb33cc080390a0f43fab65204fd3aa572b
meh <meh@schizofreni.co>
2013-07-19 06:55:18 +0200

    mix: bump Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Continuum.Mixfile do
   def project do
     [ app: :continuum,
       version: "0.0.1",
-      elixir: "~> 0.9.4-dev",
+      elixir: "~> 0.10.0",
       deps: deps ]
   end
 
