260155683464f34ea06885b3f91933cee99679df
meh <meh@schizofreni.co>
2014-06-07 17:24:12 +0200

    mix: support Elixir master

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Continuum.Mixfile do
   def project do
     [ app: :continuum,
       version: "0.1.0-dev",
-      elixir: "~> 0.13.0",
+      elixir: "~> 0.14.0-dev",
       deps: deps ]
   end
 
